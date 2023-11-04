75237a32dd4f040a37710b60b7a7821a4ca041c2
SO HUI <haejuuun925@gmail.com>
2023-11-05 05:10:12 +0900

    feat: User, User 함수형 컴포넌트를 클래스 컴포넌트로 업데이트

--- class-based-component/src/components/Users.js
+++ class-based-component/src/components/Users.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import classes from './Users.module.css';
 import User from './User';
 
@@ -8,29 +8,40 @@ const DUMMY_USERS = [
     { id: 'u3', name: 'Julie' }
 ];
 
-const Users = () => {
-    const [showUsers, setShowUsers] = useState(true);
+class Users extends Component {
+    // 클래스 컴포넌트에서 상태 정의 시에는 생성자 사용.
+    constructor() {
+        super();
+        this.state = {
+            showUsers: true
+        };
+    }
 
-    const toggleUserHandler = () => {
-        setShowUsers((curState) => !curState);
+    toggleUserHandler() {
+        this.setState((curState) => {
+            return { showUsers: !curState.showUsers };
+        });
     };
 
-    const userList = (
-        <ul>
-            {DUMMY_USERS.map((user) => (
-                <User key={user.id} name={user.name} />
-            ))}
-        </ul>
-    )
+    render() {
+        const userList = (
+            <ul>
+                {DUMMY_USERS.map((user) => (
+                    <User key={user.id} name={user.name} />
+                ))}
+            </ul>
+        );
 
-    return (
-        <div className={classes.users}>
-            <button onClick={toggleUserHandler}>
-                {showUsers ? 'Hide' : 'Show'} Users
-            </button>
-            {showUsers && userList}
-        </div>
-    );
-};
+        return (
+            <div className={classes.users}>
+                {/* bind(this)에서 this 예약어는 이 클래스를 참조한다는 의미. */}
+                <button onClick={this.toggleUserHandler.bind(this)}>
+                    {this.state.showUsers ? 'Hide' : 'Show'} Users
+                </button>
+                {this.state.showUsers && userList}
+            </div>
+        );
+    }
+}
 
 export default Users;
