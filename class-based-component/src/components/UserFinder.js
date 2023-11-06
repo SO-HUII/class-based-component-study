297fb65c936a6cc6da102e4717eb8dc4d35bf765
SO HUI <haejuuun925@gmail.com>
2023-11-06 22:59:50 +0900

    feat: UserFinder, user-context 구현 및 스타일링

---
+++ class-based-component/src/components/UserFinder.js
@@ -0,0 +1,65 @@
+import { Fragment, Component } from 'react';
+
+import Users from './Users';
+import classes from './UserFinder.module.css';
+import UsersContext from '../store/user-context';
+
+class UserFinder extends Component {
+    static contextType = UsersContext;
+    constructor() {
+        super();
+        this.state = {
+            filteredUsers: [],
+            searchTerm: ''
+        };
+    }
+
+    componentDidMount() {
+        // Send http request..
+        this.setState({ filteredUsers: this.context.users });
+    }
+
+    // 상태 변화로 인해 컴포넌트 재평가 시, 리액트에 의해 자동적으로 호출됨.
+    componentDidUpdate(prevProps, prevState) {
+        // 무한루프 방지를 위해 상태가 바뀌었는지 확인 필요. (searchTerm의 변경 여부)
+        if(prevState.searchTerm !== this.state.searchTerm) {
+            this.setState({
+                filteredUsers: this.context.users.filter((user) => 
+                    user.name.includes(this.state.searchTerm)
+                ),
+            });
+        }
+    }
+
+    searchChangeHandler(event) {
+        this.setState({searchTerm: event.target.value});
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <div className={classes.finder}>
+                    <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+                </div>
+                <Users users={this.state.filteredUsers} />
+            </Fragment>
+        );
+    }
+}
+
+// const UserFinder = () => {
+//     const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//     const [searchTerm, setSearchTerm] = useState('');
+
+//     useEffect(() => {
+//         setFilteredUsers(
+//             DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//         );
+//     }, [searchTerm]);
+
+//     const searchChangeHandler = (event) => {
+//         setSearchTerm(event.target.value);
+//     };   
+// };
+
+export default UserFinder;
